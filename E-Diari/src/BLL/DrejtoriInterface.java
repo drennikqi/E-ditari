7efda7a2bd264876538df5e3bb0f314e5f5ac902
ArtonRamadani1 <76445444+ArtonRamadani1@users.noreply.github.com>
2021-01-14 11:30:01 +0100

    Add files via upload

---
+++ E-Diari/src/BLL/DrejtoriInterface.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BLL;
+
+import Entities.Drejtori;
+import java.util.List;
+
+/**
+ *
+ * @author dreni
+ */
+public interface DrejtoriInterface {
+    void create(Drejtori d) throws CrudFormException;
+    void edit(Drejtori d) throws CrudFormException;
+    void delete(Drejtori d) throws CrudFormException;
+    List<Drejtori> findAll() throws CrudFormException;
+    Drejtori findByID(Integer ID) throws CrudFormException;
+}
